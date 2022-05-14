363273619d07431243b68ca4e9bfc40b5ed491fe
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2022-05-14 13:20:16 +0900

    iInitial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM_GitHubAPI"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/kmm_githubapi/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.kmm_githubapi
+
+expect class Platform() {
+    val platform: String
+}
